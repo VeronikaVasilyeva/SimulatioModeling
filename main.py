95501c0c9b20f743155e109fd9f3a34b361afb97
WeslyG <Weslyg22@gmail.com>
2018-06-20 23:51:41 +0500

    commit

--- main.py
+++ main.py
@@ -1,46 +1,82 @@
 import argparse
-import os
-import shutil
-import time
-import numpy
-from queue import Queue
+import random
 
-# from file import
+parser = argparse.ArgumentParser(description='Simulation model')
+parser.add_argument('--L', default=200, type=int)  # ёмкость буфера
+parser.add_argument('--Nmax', default=100000, type=int)
 
-parser = argparse.ArgumentParser(description='PyTorch CelebA Training')
-parser.add_argument('-j', '--workers', default=16, type=int, metavar='N',
-                    help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=60, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                    metavar='LR', help='initial learning rate')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('--pretrained', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                    help='evaluate model on validation set')
-parser.add_argument('--end2end', action='store_true', \
-                    help='if true, using end2end with dream block, else, using naive architecture')
-
-seed = 0  # начальное значение гсч
-appear_time = 0  # среднее время появления между заявками
-service_time = 0  # среднее время обслуживания
+appear_time = 3  # среднее время появления между заявками
+service_time = 9  # среднее время обслуживания
 loss_probability = 0  # коэффициент (вероятность) потерь
 len_queue = 0  # средняя длина очереди
-L_queue = 0  # ёмкость буфера
 model_time = 0  # абсолютное время жизни модели
-queue = Queue()             # буфер сообщений q.put('eat') print(q.get())
-message_number = 0          # количество сообщений в модели, переменная состояния
-current_mess_number = 0     # текущее кол-во постопивших заявок
-
-def main():
-    global args
-    args = parser.parse_args()
-
-    print('img_dir:', args.img_dir)
-    print('end2end?:', args.end2end)
+n = 0  # количество заявок в модели, переменная состояния
+current_issue_number = 0  # текущее кол-во поступивших заявок
+loss_issue_number = 0  # кол-во потерянных заявок
+Tin = 0  # время вхда
+Tout = 0  # время выхода
+Nmax = 0
+L = 0
 
 
-if __name__ == '__main__':
-    main()
+def increment_state():
+    global n
+    n += 1
+
+
+def is_end():
+    global n, current_issue_number, Tin, Tout, loss_issue_number, loss_probability, Nmax, L
+
+    if current_issue_number == Nmax:
+        loss_probability = loss_issue_number / Nmax
+        return True
+    else:
+        return False
+
+
+def handle_issues():
+    global L, Tin, Tout, n, loss_issue_number
+    if Tin < Tout:
+        if n == L + 1:
+            loss_issue_number += 1
+        else:
+            increment_state()
+        create_issue()
+        return
+    else:
+        n -= 1
+        if n == 0:
+            Tout = Tin + random.expovariate(1 / service_time)
+            increment_state()
+            create_issue()
+            return
+        else:
+            Tout = Tout + random.expovariate(1 / service_time)
+            handle_issues()
+
+
+def create_issue():
+    global current_issue_number, Tin
+    current_issue_number += 1
+    Tin = Tin + random.expovariate(1 / appear_time)
+
+# ======================MAIN=============================
+args = parser.parse_args()
+Nmax, L = args.Nmax, args.L
+Tin = random.expovariate(1 / appear_time)
+Tout = Tin + random.expovariate(1 / service_time)
+
+increment_state()
+create_issue()
+i = 1
+while not is_end():
+    len_queue += n
+    i += 1
+    handle_issues()
+
+print('Общее число заявок:', Nmax)
+print('Размер буффера:', L)
+print('Коэффициент(вероятность) потерь:', float(loss_probability))
+print('Средняя длина очереди:', len_queue / i)
+exit()
+
